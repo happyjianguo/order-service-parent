deda3bac42e8571b35aa8002f46c8212c92a4948
jinxu <www.191936793@qq.com>
2020-07-02 20:06:00 +0800

    修改交易类型为string类型

--- order-service-sdk/src/main/java/com/dili/order/domain/TransitionDepartureSettlement.java
+++ order-service-sdk/src/main/java/com/dili/order/domain/TransitionDepartureSettlement.java
@@ -467,7 +467,7 @@ public class TransitionDepartureSettlement extends BaseDomain {
      *
      * @return trans_type_id - 交易类型id（外省菜，省内菜等）（数据字典）
      */
-    @FieldDef(label = "交易类型id（外省菜，省内菜等）（数据字典）")
+    @FieldDef(label = "交易类型id（外省菜，省内菜等）（数据字典）",maxLength = 20)
     @EditMode(editor = FieldEditor.Text, required = false)
     public String getTransTypeId() {
         return transTypeId;
