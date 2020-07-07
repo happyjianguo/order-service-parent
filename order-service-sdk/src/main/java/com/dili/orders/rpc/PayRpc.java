9a4ec9ec18417a9db3250fb82fd2a072dbe9ee39
jinxu <www.191936793@qq.com>
2020-07-07 16:25:15 +0800

    新增缴费rpc方法

--- order-service-sdk/src/main/java/com/dili/orders/rpc/PayRpc.java
+++ order-service-sdk/src/main/java/com/dili/orders/rpc/PayRpc.java
@@ -21,53 +21,63 @@ import feign.Headers;
 @FeignClient(name = "pay-service", contextId = "pay")
 public interface PayRpc {
 
-	/**
-	 * 查询余额
-	 * 
-	 * @param query
-	 * @return
-	 */
-	@Headers({ "Content-Type:application/json", "appid:" + OrdersConstant.PAYMENT_APP_ID, "token:" + OrdersConstant.PAYMENT_TOKEN, "service:" + OrdersConstant.PAYMENT_FUND_SERVICE_QUERY })
-	@RequestMapping(value = "/payment/api/gateway.do", method = RequestMethod.POST)
-	BaseOutput<AccountBalanceDto> queryAccountBalance(@RequestBody AccountRequestDto query);
+    /**
+     * 查询余额
+     *
+     * @param query
+     * @return
+     */
+    @Headers({"Content-Type:application/json", "appid:" + OrdersConstant.PAYMENT_APP_ID, "token:" + OrdersConstant.PAYMENT_TOKEN, "service:" + OrdersConstant.PAYMENT_FUND_SERVICE_QUERY})
+    @RequestMapping(value = "/payment/api/gateway.do", method = RequestMethod.POST)
+    BaseOutput<AccountBalanceDto> queryAccountBalance(@RequestBody AccountRequestDto query);
 
-	/**
-	 * 创建支付订单
-	 * 
-	 * @param dto
-	 * @return 交易号
-	 */
-	@Headers({ "Content-Type:application/json", "appid:" + OrdersConstant.PAYMENT_APP_ID, "token:" + OrdersConstant.PAYMENT_TOKEN, "service:" + OrdersConstant.PAYMENT_TRADE_SERVICE_PREPARE })
-	@RequestMapping(value = "/payment/api/gateway.do", method = RequestMethod.POST)
-	BaseOutput<String> prepare(@RequestBody PaymentTradePrepareDto dto);
+    /**
+     * 创建支付订单
+     *
+     * @param dto
+     * @return 交易号
+     */
+    @Headers({"Content-Type:application/json", "appid:" + OrdersConstant.PAYMENT_APP_ID, "token:" + OrdersConstant.PAYMENT_TOKEN, "service:" + OrdersConstant.PAYMENT_TRADE_SERVICE_PREPARE})
+    @RequestMapping(value = "/payment/api/gateway.do", method = RequestMethod.POST)
+    BaseOutput<String> prepare(@RequestBody PaymentTradePrepareDto dto);
 
-	/**
-	 * 提交预授权交易
-	 * 
-	 * @param dto
-	 * @return 交易号
-	 */
-	@Headers({ "Content-Type:application/json", "appid:" + OrdersConstant.PAYMENT_APP_ID, "token:" + OrdersConstant.PAYMENT_TOKEN, "service:" + OrdersConstant.PAYMENT_TRADE_SERVICE_CONFIRM })
-	@RequestMapping(value = "/payment/api/gateway.do", method = RequestMethod.POST)
-	BaseOutput<PaymentTradeCommitResponseDto> confirm(@RequestBody PaymentTradeConfirmDto dto);
+    /**
+     * 提交预授权交易
+     *
+     * @param dto
+     * @return 交易号
+     */
+    @Headers({"Content-Type:application/json", "appid:" + OrdersConstant.PAYMENT_APP_ID, "token:" + OrdersConstant.PAYMENT_TOKEN, "service:" + OrdersConstant.PAYMENT_TRADE_SERVICE_CONFIRM})
+    @RequestMapping(value = "/payment/api/gateway.do", method = RequestMethod.POST)
+    BaseOutput<PaymentTradeCommitResponseDto> confirm(@RequestBody PaymentTradeConfirmDto dto);
 
-	/**
-	 * 撤销预授权交易
-	 * 
-	 * @param dto
-	 * @return 交易号
-	 */
-	@Headers({ "Content-Type:application/json", "appid:" + OrdersConstant.PAYMENT_APP_ID, "token:" + OrdersConstant.PAYMENT_TOKEN, "service:" + OrdersConstant.PAYMENT_TRADE_SERVICE_CANCEL })
-	@RequestMapping(value = "/payment/api/gateway.do", method = RequestMethod.POST)
-	BaseOutput<PaymentTradeCommitResponseDto> confirm(@RequestBody PaymentTradeCancelDto dto);
+    /**
+     * 撤销预授权交易
+     *
+     * @param dto
+     * @return 交易号
+     */
+    @Headers({"Content-Type:application/json", "appid:" + OrdersConstant.PAYMENT_APP_ID, "token:" + OrdersConstant.PAYMENT_TOKEN, "service:" + OrdersConstant.PAYMENT_TRADE_SERVICE_CANCEL})
+    @RequestMapping(value = "/payment/api/gateway.do", method = RequestMethod.POST)
+    BaseOutput<PaymentTradeCommitResponseDto> confirm(@RequestBody PaymentTradeCancelDto dto);
 
-	/**
-	 * 提交即时交易
-	 * 
-	 * @param dto
-	 * @return 交易号
-	 */
-	@Headers({ "Content-Type:application/json", "appid:" + OrdersConstant.PAYMENT_APP_ID, "token:" + OrdersConstant.PAYMENT_TOKEN, "service:" + OrdersConstant.PAYMENT_TRADE_SERVICE_COMMIT })
-	@RequestMapping(value = "/payment/api/gateway.do", method = RequestMethod.POST)
-	BaseOutput<PaymentTradeCommitResponseDto> commit(@RequestBody PaymentTradeCommitDto dto);
+    /**
+     * 提交即时交易
+     *
+     * @param dto
+     * @return 交易号
+     */
+    @Headers({"Content-Type:application/json", "appid:" + OrdersConstant.PAYMENT_APP_ID, "token:" + OrdersConstant.PAYMENT_TOKEN, "service:" + OrdersConstant.PAYMENT_TRADE_SERVICE_COMMIT})
+    @RequestMapping(value = "/payment/api/gateway.do", method = RequestMethod.POST)
+    BaseOutput<PaymentTradeCommitResponseDto> commit(@RequestBody PaymentTradeCommitDto dto);
+
+    /**
+     * 缴费
+     *
+     * @param dto
+     * @return 交易号
+     */
+    @Headers({"Content-Type:application/json", "appid:" + OrdersConstant.PAYMENT_APP_ID, "token:" + OrdersConstant.PAYMENT_TOKEN, "service:" + OrdersConstant.PAYMENT_TRADE_SERVICE_PAY})
+    @RequestMapping(value = "/payment/api/gateway.do", method = RequestMethod.POST)
+    BaseOutput<PaymentTradeCommitResponseDto> pay(@RequestBody PaymentTradeCommitDto dto);
 }
