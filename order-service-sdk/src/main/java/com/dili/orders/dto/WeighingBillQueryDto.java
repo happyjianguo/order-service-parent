dec08543542e1460c52d5151e9aafc70234dafc9
jiang <jiang@DESKTOP-GJ4LO3T>
2020-07-08 16:53:21 +0800

    新增过磅单返回结算单信息，删除无用的mapper.xml文件

--- order-service-sdk/src/main/java/com/dili/orders/dto/WeighingBillQueryDto.java
+++ order-service-sdk/src/main/java/com/dili/orders/dto/WeighingBillQueryDto.java
@@ -1,5 +1,6 @@
 package com.dili.orders.dto;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import javax.persistence.Column;
@@ -10,11 +11,21 @@ import com.dili.ss.domain.annotation.Operator;
 public class WeighingBillQueryDto extends WeighingBill {
 
 	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -789812465777179417L;
+	/**
 	 * 状态
 	 */
 	@Operator(Operator.IN)
 	@Column(name = "`state`")
 	private List<Integer> states;
+	@Operator(Operator.GREAT_EQUAL_THAN)
+	@Column(name = "`created_time`")
+	private LocalDateTime createdStart;
+	@Operator(Operator.LITTLE_EQUAL_THAN)
+	@Column(name = "`created_time`")
+	private LocalDateTime createdEnd;
 
 	public List<Integer> getStates() {
 		return states;
@@ -22,6 +33,22 @@ public class WeighingBillQueryDto extends WeighingBill {
 
 	public void setStates(List<Integer> states) {
 		this.states = states;
+	}
+
+	public LocalDateTime getCreatedStart() {
+		return createdStart;
+	}
+
+	public void setCreatedStart(LocalDateTime createdStart) {
+		this.createdStart = createdStart;
+	}
+
+	public LocalDateTime getCreatedEnd() {
+		return createdEnd;
+	}
+
+	public void setCreatedEnd(LocalDateTime createdEnd) {
+		this.createdEnd = createdEnd;
 	}
 
 }
