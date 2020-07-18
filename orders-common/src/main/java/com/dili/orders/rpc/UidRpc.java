6426ed3613c64ef4ac6767be4976990868043b58
jinxu <www.191936793@qq.com>
2020-07-18 09:42:47 +0800

    新增uid参数

--- orders-common/src/main/java/com/dili/orders/rpc/UidRpc.java
+++ orders-common/src/main/java/com/dili/orders/rpc/UidRpc.java
@@ -5,11 +5,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.dili.ss.domain.BaseOutput;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "dili-uid", contextId = "uidRpc")
 public interface UidRpc {
 
-	@RequestMapping(value = "/api/bizNumber/sid53", method = RequestMethod.GET)
-	BaseOutput<String> getCode();
+    @RequestMapping(value = "/api/bizNumber/sid53", method = RequestMethod.GET)
+    BaseOutput<String> getCode();
+
+    @RequestMapping(value = "/api/bizNumber", method = RequestMethod.GET)
+    BaseOutput<String> bizNumber(@RequestParam(value = "type") String type);
 
 }
