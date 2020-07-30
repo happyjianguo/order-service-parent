0ff89c34057289de060a771894361a269c72b445
jiang <jiang@DESKTOP-GJ4LO3T>
2020-07-30 10:54:40 +0800

    解决打包报错的问题

--- orders-common/src/main/java/com/dili/orders/rpc/AssetsRpc.java
+++ orders-common/src/main/java/com/dili/orders/rpc/AssetsRpc.java
@@ -6,20 +6,19 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.dili.assets.sdk.dto.CarTypeForJmsfDTO;
+import com.dili.assets.sdk.dto.CarTypeForBusinessDTO;
 import com.dili.assets.sdk.dto.CategoryDTO;
 import com.dili.ss.domain.BaseOutput;
 
 @FeignClient(name = "assets-service", contextId = "assets")
 public interface AssetsRpc {
-    /**
-     * 获取车型
-     */
-    @RequestMapping(value = "/api/carTypePublic/listCarTypePublicByBusiness", method = RequestMethod.POST)
-    BaseOutput<List<CarTypeForJmsfDTO>> listCarType(CarTypeForJmsfDTO carTypeForJmsfDTO);
+	/**
+	 * 获取车型
+	 */
+	@RequestMapping(value = "/api/carTypePublic/listCarTypePublicByBusiness", method = RequestMethod.POST)
+	BaseOutput<List<CarTypeForBusinessDTO>> listCarType(CarTypeForBusinessDTO carTypeForJmsfDTO);
 
-    @RequestMapping(value = "/api/customCategory/getTree", method = RequestMethod.POST)
-    BaseOutput<List<CategoryDTO>> list(CategoryDTO categoryDTO);
-
+	@RequestMapping(value = "/api/customCategory/getTree", method = RequestMethod.POST)
+	BaseOutput<List<CategoryDTO>> list(CategoryDTO categoryDTO);
 
 }
