0ea84ababac4653f8be38aed03d9083240059271
jiang <jiang@DESKTOP-GJ4LO3T>
2020-09-01 15:43:17 +0800

    完成中间价审批流程

--- order-service/src/main/java/com/dili/orders/service/PriceApproveRecordService.java
+++ order-service/src/main/java/com/dili/orders/service/PriceApproveRecordService.java
@@ -2,10 +2,30 @@ package com.dili.orders.service;
 
 import com.dili.orders.domain.PriceApproveRecord;
 import com.dili.ss.base.BaseService;
+import com.dili.ss.domain.BaseOutput;
 
 /**
- * 由MyBatis Generator工具自动生成
- * This file was generated on 2020-08-30 14:29:35.
+ * 由MyBatis Generator工具自动生成 This file was generated on 2020-08-30 14:29:35.
  */
 public interface PriceApproveRecordService extends BaseService<PriceApproveRecord, Long> {
+
+	/**
+	 * 审批通过
+	 * 
+	 * @param id         id
+	 * @param approverId 审批人id
+	 * @param taskId     流程任务id
+	 * @return
+	 */
+	BaseOutput<Object> accept(Long id, Long approverId, String taskId);
+	
+	/**
+	 * 审批拒绝
+	 * 
+	 * @param id         id
+	 * @param approverId 审批人id
+	 * @param taskId     流程任务id
+	 * @return
+	 */
+	BaseOutput<Object> reject(Long id, Long approverId, String taskId);
 }
