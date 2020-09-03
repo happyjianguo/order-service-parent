ab97a234e1064f408943dc598eaff03205dd998f
jinxu <www.191936793@qq.com>
2020-09-03 15:08:31 +0800

    uid获取code的常量字段

--- order-service-sdk/src/main/java/com/dili/orders/domain/UidStatic.java
+++ order-service-sdk/src/main/java/com/dili/orders/domain/UidStatic.java
@@ -4,18 +4,18 @@ public class UidStatic {
     /**
      * 转离场申请单code
      */
-    private static final String SG_ZLC_APPLY_CODE = "sg_zlc_apply";
+    public static final String SG_ZLC_APPLY_CODE = "sg_zlc_apply";
     /**
      * 转离场结算单code
      */
-    private static final String SG_ZLC_SETTLEMENT_CODE = "sg_zlc_settlement";
+    public static final String SG_ZLC_SETTLEMENT_CODE = "sg_zlc_settlement";
     /**
      * 过磅单code
      */
-    private static final String SG_WEIGHING_BILL_CODE = "sg_weighing_bill";
+    public static final String SG_WEIGHING_BILL_CODE = "sg_weighing_bill";
     /**
      * 过磅单结算单code
      */
-    private static final String SG_WEIGHING_STATEMENT_CODE = "sg_weighing_statement";
+    public static final String SG_WEIGHING_STATEMENT_CODE = "sg_weighing_statement";
 
 }
