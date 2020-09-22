4713e96a45cbc06fc0370252460fa1b9ad37a99c
jiang <jiang@DESKTOP-GJ4LO3T>
2020-09-22 14:39:08 +0800

    更新支付错误码枚举

--- orders-common/src/main/java/com/dili/orders/dto/PaymentErrorCode.java
+++ orders-common/src/main/java/com/dili/orders/dto/PaymentErrorCode.java
@@ -16,9 +16,8 @@ public enum PaymentErrorCode {
 	INVALIDATE_STATE_EXCEPTION("502004", "无效的交易状态"),
 	FUND_ACCOUNT_NOT_FOUND_EXCEPTION("503001", "资金账号不存在"), 
 	INVALIDATE_FOUND_ACCOUNT_STATE_EXCEPTION("503002", "无效资金账号状态"), 
-	ACCOUNT_PASSWORD_INCORRECT_EXCEPTION("503002", "账号密码不正确"),
-	LACK_OF_BALANCE_EXCEPTION("503003", "账户余额不足"),
-	INVALIDATE_FOUND_STATE_EXCEPTION("503002", "无效资金状态");
+	ACCOUNT_PASSWORD_INCORRECT_EXCEPTION("503003", "账号密码不正确"),
+	LACK_OF_BALANCE_EXCEPTION("503004", "账户余额不足");
 
 	private PaymentErrorCode(String code, String name) {
 		this.code = code;
