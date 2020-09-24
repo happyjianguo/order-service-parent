08fbf3afb3ce5154b9ced425ac17f22bf8fe63dd
jinxu <www.191936793@qq.com>
2020-09-24 15:03:40 +0800

    修改查询交易类型顺序

--- order-service/src/main/java/com/dili/orders/service/impl/TransitionDepartureSettlementServiceImpl.java
+++ order-service/src/main/java/com/dili/orders/service/impl/TransitionDepartureSettlementServiceImpl.java
@@ -274,6 +274,12 @@ public class TransitionDepartureSettlementServiceImpl extends BaseServiceImpl<Tr
         if (!pwdOutput.isSuccess()) {
             return pwdOutput;
         }
+        //根据交易类型id获取交易类型
+        BaseOutput<TradeTypeDto> tradeTypeDtoBaseOutput = this.tradeTypeRpc.get(Long.valueOf(transitionDepartureSettlement.getTransTypeId()));
+        if (!tradeTypeDtoBaseOutput.isSuccess()) {
+            return BaseOutput.failure(tradeTypeDtoBaseOutput.getMessage());
+        }
+        transitionDepartureSettlement.setTransTypeName(tradeTypeDtoBaseOutput.getData().getName());
         //设置余额
         transitionDepartureSettlement.setCustomerBalance(String.valueOf(accountFund.getBalance() / 100));
         //设置为已支付状态
@@ -300,13 +306,6 @@ public class TransitionDepartureSettlementServiceImpl extends BaseServiceImpl<Tr
         if (i <= 0) {
             return BaseOutput.failure("修改申请单状态失败");
         }
-        //根据交易类型id获取交易类型
-        BaseOutput<TradeTypeDto> tradeTypeDtoBaseOutput = this.tradeTypeRpc.get(Long.valueOf(transitionDepartureSettlement.getTransTypeId()));
-        if (!tradeTypeDtoBaseOutput.isSuccess()) {
-            return BaseOutput.failure(tradeTypeDtoBaseOutput.getMessage());
-        }
-        transitionDepartureSettlement.setTransTypeName(tradeTypeDtoBaseOutput.getData().getName());
-
         //设置支付时间
         transitionDepartureSettlement.setPayTime(LocalDateTime.now());
 
