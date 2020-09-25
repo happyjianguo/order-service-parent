48989555b29dff8cf60f6264a6446da53dc46c89
SIT_Yangloft <1qaz2wsx>
2020-09-25 14:43:25 +0800

    回退版本

--- order-service/src/main/java/com/dili/orders/api/ReferencePriceApi.java
+++ order-service/src/main/java/com/dili/orders/api/ReferencePriceApi.java
@@ -3,22 +3,20 @@ package com.dili.orders.api;
 
 import com.dili.orders.service.ReferencePriceService;
 import com.dili.ss.domain.BaseOutput;
-import com.dili.ss.exception.AppException;
 import com.dili.ss.util.MoneyUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Objects;
 
 
 /**
- * 参考价获取接口类
+ * 由MyBatis Generator工具自动生成
+ * This file was generated on 2020-06-17 08:52:43.
  * @author Tyler
  * @date 2020年8月17日10:18:00
  */
@@ -29,6 +27,9 @@ public class ReferencePriceApi {
 
     @Autowired
     ReferencePriceService referencePriceService;
+
+    @Autowired
+    private AmqpTemplate rabbitMQTemplate;
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ReferencePriceApi.class);
 
@@ -52,7 +53,7 @@ public class ReferencePriceApi {
                 return BaseOutput.successData(0);
             }
             return BaseOutput.successData(MoneyUtils.centToYuan(referencePrice));
-        } catch (AppException e) {
+        } catch (Exception e) {
             LOGGER.error("获取商品参考价异常："+e.getMessage());
             return BaseOutput.failure("500", "获取失败");
         }
