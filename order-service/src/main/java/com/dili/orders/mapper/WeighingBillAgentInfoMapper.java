f0e9a8a2f679ff2b182cbd64f88e4db60c4e195c
jiang <jiang@DESKTOP-GJ4LO3T>
2020-09-28 09:25:41 +0800

    中间价审批模型增加市场字段，过磅单结算单模块增加代理人实体

---
+++ order-service/src/main/java/com/dili/orders/mapper/WeighingBillAgentInfoMapper.java
@@ -0,0 +1,7 @@
+package com.dili.orders.mapper;
+
+import com.dili.orders.domain.WeighingBillAgentInfo;
+import com.dili.ss.base.MyMapper;
+
+public interface WeighingBillAgentInfoMapper extends MyMapper<WeighingBillAgentInfo> {
+}
