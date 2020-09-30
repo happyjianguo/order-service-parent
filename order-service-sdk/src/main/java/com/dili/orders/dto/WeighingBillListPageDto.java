c0d51c3d8f643e1c204997af1e36a8c0517ca24f
jiang <jiang@DESKTOP-GJ4LO3T>
2020-09-30 10:18:52 +0800

    C端接口增加过磅员字段

--- order-service-sdk/src/main/java/com/dili/orders/dto/WeighingBillListPageDto.java
+++ order-service-sdk/src/main/java/com/dili/orders/dto/WeighingBillListPageDto.java
@@ -1,12 +1,18 @@
 package com.dili.orders.dto;
 
+import java.time.LocalDateTime;
+
 import javax.persistence.Transient;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
+import com.alibaba.fastjson.annotation.JSONField;
 import com.dili.orders.domain.MeasureType;
 import com.dili.orders.domain.WeighingBill;
 import com.dili.orders.domain.WeighingBillOperationRecord;
 import com.dili.orders.domain.WeighingStatement;
 import com.dili.ss.util.MoneyUtils;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 public class WeighingBillListPageDto extends WeighingBill {
 
@@ -47,6 +53,13 @@ public class WeighingBillListPageDto extends WeighingBill {
 			return null;
 		}
 		return MoneyUtils.centToYuan(this.getUnitPrice());
+	}
+
+	@DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+	@JSONField(format = "yyyy-MM-dd HH:mm:ss")
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+	public LocalDateTime getWeighingOperationTime() {
+		return super.getModifiedTime() != null ? super.getModifiedTime() : super.getCreatedTime();
 	}
 
 }
