483893c1d81bace733840c678965ea452eee4396
jiang <jiang@DESKTOP-GJ4LO3T>
2020-10-30 18:04:13 +0800

    解决报错

--- orders-common/src/main/java/com/dili/orders/utils/WebUtil.java
+++ orders-common/src/main/java/com/dili/orders/utils/WebUtil.java
@@ -1,15 +1,7 @@
 package com.dili.orders.utils;
 
-import java.sql.Timestamp;
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.servlet.http.HttpServletRequest;
 
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.stereotype.Component;
-
-@Component
 public class WebUtil {
 
 	/**
