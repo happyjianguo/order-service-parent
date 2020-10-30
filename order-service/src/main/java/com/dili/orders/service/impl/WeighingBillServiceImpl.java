c51411e580deca899ddc4c1a1333b4663693c676
jiang <jiang@DESKTOP-GJ4LO3T>
2020-10-30 18:35:31 +0800

    解决价格计算精度问题

--- order-service/src/main/java/com/dili/orders/service/impl/WeighingBillServiceImpl.java
+++ order-service/src/main/java/com/dili/orders/service/impl/WeighingBillServiceImpl.java
@@ -1381,7 +1381,7 @@ public class WeighingBillServiceImpl extends BaseServiceImpl<WeighingBill, Long>
 				map.put("totalWeight", new BigDecimal(MoneyUtils.centToYuan(weighingBill.getNetWeight())));
 			} else {
 				// 斤转换为公斤
-				map.put("unitPrice", new BigDecimal(getConvertUnitPrice(weighingBill) * 2).divide(new BigDecimal(100), 2, RoundingMode.HALF_UP));
+				map.put("unitPrice", new BigDecimal(getConvertDoubleUnitPrice(weighingBill)).divide(new BigDecimal(100), 2, RoundingMode.HALF_UP));
 				map.put("totalWeight", new BigDecimal(weighingBill.getUnitAmount() * weighingBill.getUnitWeight()).divide(new BigDecimal(200)).setScale(2, RoundingMode.HALF_UP));
 			}
 			map.put("tradeTypeId", weighingBill.getTradeTypeId());
@@ -1486,6 +1486,18 @@ public class WeighingBillServiceImpl extends BaseServiceImpl<WeighingBill, Long>
 		} else {
 			// 转换为斤的价格，保留到分，四舍五入
 			actualPrice = new BigDecimal(weighingBill.getUnitPrice()).divide(new BigDecimal(weighingBill.getUnitWeight()), 2, RoundingMode.HALF_UP).multiply(new BigDecimal(100)).longValue();
+		}
+		return actualPrice;
+	}
+
+	// 获取单价，如果按件计价需要转换为按公斤斤计价
+	private Long getConvertDoubleUnitPrice(WeighingBill weighingBill) {
+		Long actualPrice = null;
+		if (weighingBill.getMeasureType().equals(MeasureType.WEIGHT.getValue())) {
+			actualPrice = weighingBill.getUnitPrice() * 2;
+		} else {
+			// 转换为斤的价格，保留到分，四舍五入
+			actualPrice = new BigDecimal(weighingBill.getUnitPrice() * 2).divide(new BigDecimal(weighingBill.getUnitWeight()), 2, RoundingMode.HALF_UP).multiply(new BigDecimal(100)).longValue();
 		}
 		return actualPrice;
 	}
