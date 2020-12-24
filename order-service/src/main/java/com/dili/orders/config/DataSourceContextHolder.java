74a4db616c03f2c241c7a5341deef02c85378fc9
jiang <jiang@DESKTOP-GJ4LO3T>
2020-12-24 17:26:19 +0800

    增加注释

--- order-service/src/main/java/com/dili/orders/config/DataSourceContextHolder.java
+++ order-service/src/main/java/com/dili/orders/config/DataSourceContextHolder.java
@@ -15,15 +15,27 @@ public class DataSourceContextHolder {
 
 	private static final ThreadLocal<String> contextHolder = new ThreadLocal<>();
 
-	// 设置数据源类型
+	/**
+	 * 设置数据源类型
+	 * 
+	 * @param dbType
+	 */
 	public static void setDbType(String dbType) {
 		contextHolder.set(dbType);
 	}
 
+	/**
+	 * 获取数据源类型
+	 * 
+	 * @return
+	 */
 	public static String getDbType() {
 		return contextHolder.get();
 	}
 
+	/**
+	 * 重置
+	 */
 	public static void clearDbType() {
 		contextHolder.remove();
 	}
