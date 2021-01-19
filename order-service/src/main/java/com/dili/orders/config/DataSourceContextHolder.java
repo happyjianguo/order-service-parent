2f3c9ddb384b629f0030da1d7e2455fc30bdee81
jinxu <www.191936793@qq.com>
2021-01-19 09:57:29 +0800

    数据源问题修复

--- order-service/src/main/java/com/dili/orders/config/DataSourceContextHolder.java
+++ order-service/src/main/java/com/dili/orders/config/DataSourceContextHolder.java
@@ -1,43 +1,46 @@
 package com.dili.orders.config;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 /**
  * 数据源切换器
- * 
- * @author jiang
  *
+ * @author jiang
  */
 @Component
 @Lazy(false)
 public class DataSourceContextHolder {
 
-	private static final ThreadLocal<String> contextHolder = new ThreadLocal<>();
+    private static final ThreadLocal<String> contextHolder = new ThreadLocal<>();
 
-	/**
-	 * 设置数据源类型
-	 * 
-	 * @param dbType
-	 */
-	public static void setDbType(String dbType) {
-		contextHolder.set(dbType);
-	}
+    /**
+     * 设置数据源类型
+     *
+     * @param dbType
+     */
+    public static void setDbType(String dbType) {
+        contextHolder.set(dbType);
+    }
 
-	/**
-	 * 获取数据源类型
-	 * 
-	 * @return
-	 */
-	public static String getDbType() {
-		return contextHolder.get();
-	}
+    /**
+     * 获取数据源类型
+     *
+     * @return
+     */
+    public static String getDbType() {
+        if (StringUtils.isBlank(contextHolder.get())) {
+            return "updateDataSource";
+        }
+        return contextHolder.get();
+    }
 
-	/**
-	 * 重置
-	 */
-	public static void clearDbType() {
-		contextHolder.remove();
-	}
+    /**
+     * 重置
+     */
+    public static void clearDbType() {
+        contextHolder.remove();
+    }
 
 }
