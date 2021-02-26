00e81078dd78cc08c82e2a92a6852418f1fe12ed
jiang <jiang@DESKTOP-GJ4LO3T>
2021-02-26 10:11:58 +0800

    解决包装名称未显示的问题

--- order-service-sdk/src/main/java/com/dili/orders/dto/WeighingCollectionStatementDto.java
+++ order-service-sdk/src/main/java/com/dili/orders/dto/WeighingCollectionStatementDto.java
@@ -5,148 +5,159 @@ import com.dili.orders.domain.WeighingStatement;
 import java.math.BigDecimal;
 
 public class WeighingCollectionStatementDto extends WeighingStatement {
-    //结算单id
-    private Long weighingStatementId;
+	// 结算单id
+	private Long weighingStatementId;
 
-    //结算单code
-    private String weighingStatementCode;
+	// 结算单code
+	private String weighingStatementCode;
 
-    //过磅单id
-    private Long weighingBillId;
+	// 过磅单id
+	private Long weighingBillId;
 
-    //过磅单code
-    private String weighingBillCode;
+	// 过磅单code
+	private String weighingBillCode;
 
-    //商品名称
-    private String goodsName;
+	// 商品名称
+	private String goodsName;
 
-    //净重
-    private BigDecimal netWeightY;
+	// 净重
+	private BigDecimal netWeightY;
 
-    //件数 unit_amount
-    private BigDecimal unitAmountY;
-    //货款
+	// 件数 unit_amount
+	private BigDecimal unitAmountY;
+	// 货款
 
-    private BigDecimal tradeAmountY;
+	private BigDecimal tradeAmountY;
 
-    //单价
-    private BigDecimal unitPriceY;
+	// 单价
+	private BigDecimal unitPriceY;
 
-    //人工费
-    private BigDecimal staffChargesY;
+	// 人工费
+	private BigDecimal staffChargesY;
 
-    //包装费
-    private BigDecimal packingChargesY;
+	// 包装费
+	private BigDecimal packingChargesY;
 
-    //代收费
-    private BigDecimal collectionChargesY;
+	// 代收费
+	private BigDecimal collectionChargesY;
 
-    //合计
-    private BigDecimal totalAmountY;
+	// 合计
+	private BigDecimal totalAmountY;
 
-    public Long getWeighingStatementId() {
-        return weighingStatementId;
-    }
+	// 包装名称
+	private String packingTypeName;
 
-    public void setWeighingStatementId(Long weighingStatementId) {
-        this.weighingStatementId = weighingStatementId;
-    }
+	public Long getWeighingStatementId() {
+		return weighingStatementId;
+	}
 
-    public String getWeighingStatementCode() {
-        return weighingStatementCode;
-    }
+	public void setWeighingStatementId(Long weighingStatementId) {
+		this.weighingStatementId = weighingStatementId;
+	}
 
-    public void setWeighingStatementCode(String weighingStatementCode) {
-        this.weighingStatementCode = weighingStatementCode;
-    }
+	public String getWeighingStatementCode() {
+		return weighingStatementCode;
+	}
 
-    @Override
-    public Long getWeighingBillId() {
-        return weighingBillId;
-    }
+	public void setWeighingStatementCode(String weighingStatementCode) {
+		this.weighingStatementCode = weighingStatementCode;
+	}
 
-    @Override
-    public void setWeighingBillId(Long weighingBillId) {
-        this.weighingBillId = weighingBillId;
-    }
+	@Override
+	public Long getWeighingBillId() {
+		return weighingBillId;
+	}
 
-    public String getWeighingBillCode() {
-        return weighingBillCode;
-    }
+	@Override
+	public void setWeighingBillId(Long weighingBillId) {
+		this.weighingBillId = weighingBillId;
+	}
 
-    public void setWeighingBillCode(String weighingBillCode) {
-        this.weighingBillCode = weighingBillCode;
-    }
+	public String getWeighingBillCode() {
+		return weighingBillCode;
+	}
 
-    public String getGoodsName() {
-        return goodsName;
-    }
+	public void setWeighingBillCode(String weighingBillCode) {
+		this.weighingBillCode = weighingBillCode;
+	}
 
-    public void setGoodsName(String goodsName) {
-        this.goodsName = goodsName;
-    }
+	public String getGoodsName() {
+		return goodsName;
+	}
 
-    public BigDecimal getNetWeightY() {
-        return netWeightY;
-    }
+	public void setGoodsName(String goodsName) {
+		this.goodsName = goodsName;
+	}
 
-    public void setNetWeightY(BigDecimal netWeightY) {
-        this.netWeightY = netWeightY;
-    }
+	public BigDecimal getNetWeightY() {
+		return netWeightY;
+	}
 
-    public BigDecimal getUnitAmountY() {
-        return unitAmountY;
-    }
+	public void setNetWeightY(BigDecimal netWeightY) {
+		this.netWeightY = netWeightY;
+	}
 
-    public void setUnitAmountY(BigDecimal unitAmountY) {
-        this.unitAmountY = unitAmountY;
-    }
+	public BigDecimal getUnitAmountY() {
+		return unitAmountY;
+	}
 
-    public BigDecimal getUnitPriceY() {
-        return unitPriceY;
-    }
+	public void setUnitAmountY(BigDecimal unitAmountY) {
+		this.unitAmountY = unitAmountY;
+	}
 
-    public void setUnitPriceY(BigDecimal unitPriceY) {
-        this.unitPriceY = unitPriceY;
-    }
+	public BigDecimal getUnitPriceY() {
+		return unitPriceY;
+	}
 
-    public BigDecimal getStaffChargesY() {
-        return staffChargesY;
-    }
+	public void setUnitPriceY(BigDecimal unitPriceY) {
+		this.unitPriceY = unitPriceY;
+	}
 
-    public void setStaffChargesY(BigDecimal staffChargesY) {
-        this.staffChargesY = staffChargesY;
-    }
+	public BigDecimal getStaffChargesY() {
+		return staffChargesY;
+	}
 
-    public BigDecimal getPackingChargesY() {
-        return packingChargesY;
-    }
+	public void setStaffChargesY(BigDecimal staffChargesY) {
+		this.staffChargesY = staffChargesY;
+	}
 
-    public void setPackingChargesY(BigDecimal packingChargesY) {
-        this.packingChargesY = packingChargesY;
-    }
+	public BigDecimal getPackingChargesY() {
+		return packingChargesY;
+	}
 
-    public BigDecimal getCollectionChargesY() {
-        return collectionChargesY;
-    }
+	public void setPackingChargesY(BigDecimal packingChargesY) {
+		this.packingChargesY = packingChargesY;
+	}
 
-    public void setCollectionChargesY(BigDecimal collectionChargesY) {
-        this.collectionChargesY = collectionChargesY;
-    }
+	public BigDecimal getCollectionChargesY() {
+		return collectionChargesY;
+	}
 
-    public BigDecimal getTotalAmountY() {
-        return totalAmountY;
-    }
+	public void setCollectionChargesY(BigDecimal collectionChargesY) {
+		this.collectionChargesY = collectionChargesY;
+	}
 
-    public void setTotalAmountY(BigDecimal totalAmountY) {
-        this.totalAmountY = totalAmountY;
-    }
+	public BigDecimal getTotalAmountY() {
+		return totalAmountY;
+	}
 
-    public BigDecimal getTradeAmountY() {
-        return tradeAmountY;
-    }
+	public void setTotalAmountY(BigDecimal totalAmountY) {
+		this.totalAmountY = totalAmountY;
+	}
 
-    public void setTradeAmountY(BigDecimal tradeAmountY) {
-        this.tradeAmountY = tradeAmountY;
-    }
+	public BigDecimal getTradeAmountY() {
+		return tradeAmountY;
+	}
+
+	public void setTradeAmountY(BigDecimal tradeAmountY) {
+		this.tradeAmountY = tradeAmountY;
+	}
+
+	public String getPackingTypeName() {
+		return packingTypeName;
+	}
+
+	public void setPackingTypeName(String packingTypeName) {
+		this.packingTypeName = packingTypeName;
+	}
 }
